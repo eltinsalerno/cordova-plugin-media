195c2b36dda917b954d6fb60cdf66c88187824e5
eltinsalerno <martinsalerno@gmail.com>
2014-09-03 11:28:08 -0300

    Update CDVSound.h
    add MEDIA_FINISH = 5

--- src/ios/CDVSound.h
+++ src/ios/CDVSound.h
@@ -36,7 +36,8 @@ enum CDVMediaStates {
     MEDIA_STARTING = 1,
     MEDIA_RUNNING = 2,
     MEDIA_PAUSED = 3,
-    MEDIA_STOPPED = 4
+    MEDIA_STOPPED = 4,
+    MEDIA_FINISH = 5
 };
 typedef NSUInteger CDVMediaStates;
 
